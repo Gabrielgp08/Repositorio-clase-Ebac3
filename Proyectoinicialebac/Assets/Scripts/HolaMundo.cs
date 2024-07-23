82d7c30b984511586562df1162a80088134400ec
Gabrielgp08 <161524920+Gabrielgp08@users.noreply.github.com>
2024-07-23 07:00:42 -0400

    Hola Mundo
    Se agrego un script a la main camara
    Se utilizaron las funicones Debug para imprimir los diferentes tipos de mensaje

---
+++ Proyectoinicialebac/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Algo paso");
+        Debug.Log("Algo paso");
+        Debug.LogWarning("Algo salio medianamente mal");
+        Debug.LogError("Algo salio muy mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
