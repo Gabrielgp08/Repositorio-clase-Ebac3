026d0439f501a073ee680d03ea90b078c0d9625c
Gabrielgp08 <161524920+Gabrielgp08@users.noreply.github.com>
2024-07-29 07:08:26 -0400

    Se agregaron nuevos y caracteres
    Se agrego un cube a la scene
    Se agrego nuevos scritps componente(1,2,3)
    Se actualizo el HolaMundo

--- Proyectoinicialebac/Assets/Scripts/HolaMundo.cs
+++ Proyectoinicialebac/Assets/Scripts/HolaMundo.cs
@@ -2,15 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+    int x;
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
         print("Algo paso");
-        Debug.Log("Algo paso");
+        
         Debug.LogWarning("Algo salio medianamente mal");
         Debug.LogError("Algo salio muy mal");
     }
@@ -18,6 +22,30 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
+
     }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("hola desde Fixed Update cada 50 framas");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("hola desde Late Update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("el objeto  ha sido habilitado");
+    }
+
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("el objeto ha sido inhabilitado");
+    }
+
 }
+
